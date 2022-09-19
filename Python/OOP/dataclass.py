f795c3bbcec07a048dd78b0d87307b8eea474352
mwisniewski1991 <35817164+mwisniewski1991@users.noreply.github.com>
2022-09-19 15:34:03 +0200

    dataclass.py update

--- Python/OOP/dataclass.py
+++ Python/OOP/dataclass.py
@@ -1,20 +1,26 @@
-from audioop import add
 import random
+from re import search
 import string
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 def generate_id() -> str:
     return "".join(random.choices(string.ascii_uppercase, k=12))
 
-@dataclass
+@dataclass()
 class Person:
     name: str
     address: str
+    active: bool = True
+    email_addresses: list[str]  = field(default_factory=list)
+    id: str = field(init=False, default_factory=generate_id)
+    _search_string: str = field(init=False, repr=False)
+
+    def __post_init__(self) -> None:
+        self._search_string = f'{self.name} {self.address}'
 
 def main() -> None:
-    person = Person(name='John', address='123 Main St')
+    person = Person(name='John', address='123 Main St' )
     print(person)
-
 
 if __name__ == '__main__':
     main()
