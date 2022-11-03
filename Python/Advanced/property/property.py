13375d7f28afeeaf767f1736696634d09324ef48
mwisniewski1991 <m.wisniewski1991@gmail.com>
2022-11-03 20:55:14 +0100

    Update property

--- Python/Advanced/property/property.py
+++ Python/Advanced/property/property.py
@@ -19,21 +19,21 @@ class Circle:
         print("Delete radius")
         del self._radius
 
-# circle = Circle(10.0)
+circle = Circle(10.0)
 
-# print(circle.radius)
-# circle.radius = 15
+print(circle.radius)
+circle.radius = 15
 
-# print(circle.radius)
+print(circle.radius)
 
-# del circle.radius
-# print(circle.radius)
+del circle.radius
+print(circle.radius)
 
-# Implemetning new attribute Diamater
 
+# Implemetning new attribute Diamater align with previous implementation.
 class Circle:
     def __init__(self, radius: float) -> None:
-        self.radius = radius
+        self.radius = radius #In this case _radius changes to radius.
 
     @property
     def radius(self):
@@ -53,17 +53,20 @@ class Circle:
 
     @property
     def diamater(self):
-        return self.radius * 2
+        return self.radius * 2 #Diamater property under the hood read variable radius and multiply by 2
 
     @diamater.setter
     def diamater(self, value):
-        self.radius = value / 2
+        self.radius = value / 2 #Diamater property under the hood save variable to Radius 
     
 circle = Circle(10)
-
-circle.radius = 8.5
 print(circle.radius)
 print(circle.diamater)
-circle.diamater = 80
+print("CHANGE")
+circle.radius = 8.5 #now change can be done with radius
+print(circle.radius)
+print(circle.diamater)
+print("CHANGE")
+circle.diamater = 80 # or with diamater
 print(circle.radius)
 print(circle.diamater)
--- Python/Advanced/property/property2.py
+++ Python/Advanced/property/property2.py
@@ -9,26 +9,25 @@ class Point:
         self._y = y
 
     @property
-    def x(self):
+    def x(self) -> int:
         return self._x
 
     @x.setter
-    def x(self, value):
+    def x(self, value: int) -> None:
         raise WriteCoordinateError("x coordinate is read-only")
 
     @property
-    def y(self):
+    def y(self) -> int:
         return self._y
     
-    @x.setter
-    def y(self, value):
+    @y.setter
+    def y(self, value: int) -> None:
         raise WriteCoordinateError("y coordinate is read-only")
 
     
-# point = Point(10, 5)
-# print(point.x)
-# point.x = 10
-
+point = Point(10, 5)
+print(point.x)
+point.x = 10 #This line cause error. x and y can not be change.
 
 
 # Use @property for providing Write-Only attributes
@@ -41,18 +40,18 @@ class User:
         self.password = password
 
     @property
-    def password(self):
+    def password(self) -> None:
         raise AttributeError("Password is write-only")
 
     @password.setter
-    def password(self, plaintext):
+    def password(self, plaintext: str) -> None:
         salt = os.urandom(32)
         self._hashed_password = hashlib.pbkdf2_hmac("sha256", plaintext.encode("utf-8"), salt, 100_00)
 
         
 mat = User('mat', 'pass123456')
 print(mat._hashed_password)
-# print(mat.password)
+print(mat.password) #This line cause error. Password can only be change. User can not read this.
 
 mat.password = "123"
 print(mat._hashed_password)
