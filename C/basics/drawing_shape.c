918d6e4200d05db2fb89ddb8dabafd15af138536
mwisniewski <mw.data.web@gmail.com>
2023-02-14 20:46:31 +0100

    Update C basic

---
+++ C/basics/drawing_shape.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    printf("   1   \n");
+    printf("  1 1  \n");
+    printf(" 1   1 \n");
+    printf("1     1\n");
+    printf(" 1   1 \n");
+    printf("  1 1   \n");
+    printf("   1   \n");
+
+    printf("   1   \n");
+    printf("  1 1  \n");
+    printf(" 1   1 \n");
+    printf("1     1\n");
+    printf(" 1   1 \n");
+    printf("  1 1   \n");
+    printf("   1   \n");
+    return 0;
+};
--- C/basics/hello.c
+++ C/basics/hello.c
@@ -1,33 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void hello_world(){
-        printf("Hello world\n");
-};
-
-void greet(char* name){
-        printf("Hello %s \n", name);
-};
-
-void variables(){
-
-    int age = 32;
-    printf("Age: %d \n", age);
-
-    age = 30;
-    printf("Age: %d \n", age);
-
-    int next_age = age;
-    printf("Next age: %d \n", next_age);
-
-};
-
-
 int main(){
-    // hello_world();
-    // variables();
-    char[] name = "Mateusz"
-    greet(name);
-
+    printf("Hello world\n");
+    printf("First line in C !!!!\n");
+    printf("Hello ");
+    printf("Pal");
     return 0;
 };
