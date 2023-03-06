59562d8a21e558b4e677a613c60d0d39ee95ef02
mwisniewski <mw.data.web@gmail.com>
2023-03-06 19:57:05 +0100

    Update C basics

--- C/basics/3.data_types.c
+++ C/basics/3.data_types.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 
 // int      4 bytes | %d    for printing   
+// long     8 bytes | %li   for printing 
 // float    4 bytes | %f    for printing
-// double   8 bytes | %lf   for printing
+// double   8 bytes | %lf   for printing    
 // char     1 byte  | %c    for printing  
 // char[]           | %s    for printing
 
@@ -28,12 +29,58 @@ void chars(){
     printf("%s \n", phrase);
 };
 
+void long_number(){
+    printf("Adding integers and long integers:\n");
+    int number1 = 2000000000;
+    int number2 = 2000000000;
+
+    long number3 = 2000000000;
+    long number4 = 2000000000;
+    
+    printf("Integer: %d\n", number1 + number2);
+    printf("Long Integer %li\n", number3 + number4);
+};
+
+void type_casting(){
+    printf("Type casting:\n");
+    int x = 1;
+    int y = 3;
+
+    float z = x / y;
+    printf("%f\n", z);
+
+    float c = (float) x / (float) y;
+    printf("%f\n", c);
+
+    printf("%.20f\n", c); //floating point imprecision
+
+};
+
+
+// int main(){
+
+//     numbers();
+
+//     printf("\n");
+//     chars();
+    
+//     printf("\n");
+//     long_number();
+
+//     printf("\n");
+//     type_casting();
+    
+//     return 0;
+// };
 
 int main(){
 
-    numbers();
-    printf("\n");
-    chars();
-    
-    return 0;
+    int x = 1073741824;
+    int y = 1073741824;
+
+    long x1 = 1073741824;
+    long y2 = 1073741824;
+
+    printf("%d\n", x + y);
+    printf("%li\n", x1 + y2);
 };
